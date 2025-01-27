39c8d78392ef3d3b7369bd25a7caf1d32ecd1740
Enes Hasani <enes.hasa1.1@hotmail.com>
2025-01-27 10:35:46 +0100

    Updated evaluate.py to save the confusion matrix

--- src/eval/evaluate.py
+++ src/eval/evaluate.py
@@ -1,11 +1,12 @@
 import os
+import pandas as pd
 import torch
 
 from tqdm import tqdm
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
 
-
+TOTAL_NUMBER_OF_SUBMISSIONS = 6
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 
@@ -42,12 +43,18 @@ def eval_fn(model, loader, device):
     model.eval()
 
     t = tqdm(loader)
+    confusion_matrix = torch.zeros(len(loader.dataset.classes), len(loader.dataset.classes))
     with torch.no_grad():  # no gradient needed
         for images, labels in t:
             images = images.to(device)
             labels = labels.to(device)
 
             outputs = model(images)
+            for t, p in zip(labels.view(-1), outputs.argmax(dim=1).view(-1)):
+                confusion_matrix[(t-1).long(), (p-1).long()] += 1
+            confusion_matrix = confusion_matrix / confusion_matrix.sum(1)
+            #save confusion matrix in a file
+            torch.save(confusion_matrix, 'confusion_matrix.pt')
             acc = accuracy(outputs, labels)
             score.update(acc.item(), images.size(0))
 
@@ -55,10 +62,60 @@ def eval_fn(model, loader, device):
 
     return score.avg
 
+def generate_latex_table(df, track):
+    latex_template = r"""
+\begin{{table}}
+\centering
+\caption{{{track} track leaderboard}}
+\label{{tab:results}}
+\begin{{tabular}}{{|c|c|c|c|}}
+\hline
+\textbf{{Rank}} & \textbf{{Team}} & \textbf{{Score}} & \textbf{{Nr. of submissions left}} \\
+\hline
+{table_rows}
+\hline
+\end{{tabular}}
+\end{{table}}
+"""
+    table_rows = ""
+    for _, row in df.iterrows():
+        table_rows += f"{row['Rank']} & {row['Team']} & {row['Score']} & {row['Nr. submissions left']} \\\\ \n"
 
-def eval_model(model, saved_model_file, test_data_dir, data_augmentations):
-    model = model.to(device)
-    model.load_state_dict(torch.load(os.path.join(os.getcwd(), 'models', saved_model_file)))
+    latex_code = latex_template.format(track=track, table_rows=table_rows)
+
+    with open(f'{track}_track_results.tex', 'w') as f:
+        f.write(latex_code)
+    
+    print('Latex table saved to', f'{track}_track_results.tex')
+
+def save_results(track, team_name, score) -> pd.DataFrame:
+
+    df = pd.read_csv(f'{track}_track_results.csv') if os.path.exists(f'{track}_track_results.csv') else pd.DataFrame()
+
+    if df.empty:
+        df = pd.DataFrame(columns=['Team', 'Score', 'Nr. submissions left'])
+
+    if team_name in df['Team'].values:
+        df.loc[df['Team'] == team_name, 'Score'] = score
+        df.loc[df['Team'] == team_name, 'Nr. submissions left'] -= 1
+    else:
+        new_row = pd.DataFrame({'Team': [team_name], 'Score': [score], 'Nr. submissions left': [TOTAL_NUMBER_OF_SUBMISSIONS - 1]})
+        df = pd.concat([df, new_row], ignore_index=True)
+
+    df = df.sort_values(by='Score', ascending=False).reset_index(drop=True)
+
+    df['Rank'] = df['Score'].rank(method='min', ascending=False).astype(int)
+
+    df.to_csv(f'{track}_track_results.csv', index=False)
+    print('Results saved to', f'{track}_track_results.csv')
+
+    return df
+
+def eval_model(model, saved_model_file, test_data_dir, data_augmentations) -> float:
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model.load_state_dict(torch.load(os.path.join(os.getcwd(), 'models', saved_model_file), map_location=device))
+    print("Number of parameters: ", sum(p.numel() for p in model.parameters()))
     data = ImageFolder(test_data_dir, transform=data_augmentations)
 
     test_loader = DataLoader(dataset=data,
@@ -68,3 +125,4 @@ def eval_model(model, saved_model_file, test_data_dir, data_augmentations):
     score = eval_fn(model, test_loader, device)
 
     print('Avg accuracy:', str(score*100) + '%')
+    return score
